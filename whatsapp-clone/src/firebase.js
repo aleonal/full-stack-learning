e5072932f07fdcae0f3e0b04dca77015c99097e4
Antoine <thelifeofantoine@outlook.com>
2021-01-03 22:49:46 -0700

    Created react app and added firebase keys in firebase.js

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDkbIZV6uzhsbSaM816zxTax4-Bu5Oue9c",
+    authDomain: "whatsapp-clone-1ff03.firebaseapp.com",
+    projectId: "whatsapp-clone-1ff03",
+    storageBucket: "whatsapp-clone-1ff03.appspot.com",
+    messagingSenderId: "771046584726",
+    appId: "1:771046584726:web:cb6183cc48393f30406db7",
+    measurementId: "G-XXD495LD12"
+  };
